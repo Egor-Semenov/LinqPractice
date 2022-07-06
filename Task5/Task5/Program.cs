12f01c30c96456564d56ba821ed408e711c40e7c
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-07 00:12:37 +0300

    Task5

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = GetSortedWords("Next weekend i am going to Kayaking");
+
+            list.ForEach(word => Console.WriteLine($"{word}"));
+        }
+
+        //<summary>
+        //Дан текст, нужно составить список всех встречающихся в тексте слов,
+        //упорядоченный сначала по возрастанию длины слова, а потом лексикографически.
+        //Запрещено использовать ThenBy и ThenByDescending.
+        //</summary>
+        static List<string> GetSortedWords(string text)
+        {
+            return Regex.Split(text, @"\W+")
+                .Select(word => word.ToLower())
+                .Where(word => !string.IsNullOrEmpty(word))
+                .Distinct()
+                .OrderBy(word => Tuple.Create(word.Length, word))
+                .ToList();
+        }
+    }
+}
