73456b146990cb178cb531e32023f11affd44dca
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-06 23:22:02 +0300

    Task1

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Task1
 {
@@ -6,7 +8,22 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (var num in ParseNumbers(new[] { "-0", "+0000" }))
+                Console.WriteLine(num);
+
+            foreach (var num in ParseNumbers(new List<string> { "1", "", "-03", "0" }))
+                Console.WriteLine(num);
+        }
+
+        //<summary>
+        //Вернуть массив чисел из массива строк
+        //</summary>
+        static int[] ParseNumbers(IEnumerable<string> lines)
+        {
+            return lines
+                .Where(line => line.Length > 0)
+                .Select(line => int.Parse(line))
+                .ToArray();
         }
     }
 }
