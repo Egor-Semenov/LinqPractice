9599d8cab9596e58b24cd3d5661b8c656db6bbd1
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-12 15:19:18 +0300

    Task8

---
+++ Task8/Task8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] result = ReturnStrings(new[] { "Hello", "4455", "Google" }, new[] { "Ozon", "1444" });
+
+            foreach (var word in result)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        //<summary>
+        //Даны строковые последовательности A и B.
+        //Строки последовательностей содержат только цифры и заглавные буквы латинского алфавита.
+        //Получить последовательность, содержащую все строки из A
+        //и все строки из B. Отсортировать полученную последовательность в лексикографическом порядке по убыванию.
+        //</summary>
+
+        static string[] ReturnStrings(string[] A, string[] B)
+        {
+            return A.Union(B)
+                .Where(word => !int.TryParse(word, out int number))
+                .OrderByDescending(word => word)
+                .ToArray();
+        }
+    }
+}
