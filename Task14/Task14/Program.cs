471ec11be21a0970cdb0e6a9c7d2d74d97663797
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-13 01:24:22 +0300

    Task14

---
+++ Task14/Task14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = FindCollection(new[] { 15, 25, 11, 36, 55, 15, 25 }, 5);
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        //<summary>
+        //Даны цифра D (целое однозначное число) и целочисленная последовательность A.
+        //Извлечь из A все различные положительные числа, оканчивающиеся цифрой D (в исходном порядке).
+        //При наличии повторяющихся элементов удалять все их вхождения,
+        //кроме последних.
+        //</summary>
+        static int[] FindCollection(int[] A, int D)
+        {
+            var resultArray = (from element in A
+                               where element > 0 && element % 10 == D
+                               select element).Reverse().Distinct().Reverse().ToArray();
+
+            return resultArray;
+        }
+    }
+}
