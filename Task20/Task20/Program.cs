4d8b24749e187e1b0584da46848063afdee70389
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-13 16:15:09 +0300

    Task20

---
+++ Task20/Task20/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sumList = FindSums(new List<int> { 1, 12, 25, 3, 0 }, new List<int> { 24, 12, 3, 14, 5, 25 });
+
+            foreach (var sum in sumList)
+            {
+                Console.Write($"{sum} ");
+            }
+        }
+
+        //<summary>
+        //Даны целочисленные последовательности A и B.
+        //Получить последовательность всех различных сумм,
+        //в которых первое слагаемое берется из A, а второе из B.
+        //Упорядочить полученную последовательность по возрастанию.
+        //</summary>
+        static List<int> FindSums(List<int> firstList, List<int> secondList)
+        {
+            var resultList = firstList
+                .SelectMany(firstElement =>
+                secondList.Select(secondElement => firstElement + secondElement))
+                .Distinct()
+                .OrderBy(sum => sum)
+                .ToList();
+
+            return resultList;
+        }
+    }
+}
