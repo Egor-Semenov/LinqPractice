f201402b41bbe580a64135ded639dbe384926b64
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-13 16:08:01 +0300

    Task19

---
+++ Task19/Task19/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resultList = ConcatLists(new List<int> { 2, 13, 25, 15, 5, 14, 23 }, new List<int> { 12, 18, 35, 6, 95, 17, 3 });
+
+            foreach (var pair in resultList)
+            {
+                Console.WriteLine(pair);
+            }
+        }
+
+        //<summary>
+        //Даны последовательности положительных целых чисел A и B;
+        //все числа в каждой последовательности различны.
+        //Найти последовательность всех пар чисел, удовлетворяющих следующим условиям:
+        //первый элемент пары принадлежит последовательности A,
+        //второй принадлежит B, и оба элемента оканчиваются одной и той же цифрой.
+        //</summary
+        static List<string> ConcatLists(List<int> firtsList, List<int> secondList)
+        {
+            var resultList = firtsList
+                .SelectMany(firstElement =>
+                secondList.Where(secondElement => firstElement % 10 == secondElement % 10)
+                .Select(secondElement => $"{firstElement} - {secondElement}"))
+                .ToList();
+
+            return resultList;
+        }
+    }
+}
