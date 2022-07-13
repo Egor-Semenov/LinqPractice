101cbba55dab7cecf71cf15418fc1b3f46e272ca
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-13 15:34:08 +0300

    Task16

---
+++ Task16/Task16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lastDigits = FindLastNumbersDigit(new List<int> { -15, -31, 134, 16, 1, -34, 94, 199, 5, 19, -121, 10 });
+
+            foreach (var digit in lastDigits)
+            {
+                Console.WriteLine(digit);
+            }
+        }
+
+        //<summary>
+        //Дана целочисленная последовательность.
+        //Обрабатывая только положительные числа,
+        //получить последовательность их последних цифр и
+        //удалить в полученной последовательности все вхождения одинаковых цифр, кроме первого.
+        //Порядок полученных цифр должен соответствовать порядку исходных чисел.
+        //</summary>
+        static List<int> FindLastNumbersDigit(List<int> data)
+        {
+            var lastDigits = (from number in data
+                              where number > 0
+                              select number % 10).Distinct().ToList();
+
+            return lastDigits;
+        }
+    }
+}
