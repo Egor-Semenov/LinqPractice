baf377d9649e1992b81e7093992f25a7f73fbb47
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-13 15:27:02 +0300

    Task15

---
+++ Task15/Task15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> data = GetFirstCharacters(new List<string>() { "first", "last", "my", "game" });
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        //<summary>
+        //Дана последовательность непустых строк A.
+        //Получить последовательность символов,
+        //каждый элемент которой является начальным символом соответствующей строки из A.
+        //Порядок символов должен быть обратным по отношению к порядку элементов исходной последовательности.
+        //</summary>
+        static List<char> GetFirstCharacters(List<string> stringList) 
+        {
+            var resultList = (from word in stringList
+                              select word[0]).Reverse().ToList();
+
+            return resultList;
+        } 
+    }
+}
