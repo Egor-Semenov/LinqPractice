16035946db24f1b2b3d35255a4c36f4602543c8c
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-15 00:21:55 +0300

    Task24

--- Task24/Task24/Program.cs
+++ Task24/Task24/Program.cs
@@ -19,16 +19,21 @@ namespace Task24
             {
                 new Client {Id = 1, Year = 2019, Month = 10, OverallTrainingTime = 30 },
                 new Client {Id = 2, Year = 2017, Month = 11, OverallTrainingTime = 36 },
-                new Client {Id = 3, Year = 2019, Month = 11, OverallTrainingTime = 36 },
+                new Client {Id = 4, Year = 2016, Month = 1, OverallTrainingTime = 30 },
+                new Client {Id = 1, Year = 2018, Month = 12, OverallTrainingTime = 50 },
+                new Client {Id = 3, Year = 2015, Month = 12, OverallTrainingTime = 37 },
+                new Client {Id = 2, Year = 2019, Month = 18, OverallTrainingTime = 48 },
+                new Client {Id = 1, Year = 2015, Month = 7, OverallTrainingTime = 33 },
+                new Client {Id = 3, Year = 2019, Month = 6, OverallTrainingTime = 36 },
                 new Client {Id = 4, Year = 2017, Month = 12, OverallTrainingTime = 30 },
                 new Client {Id = 5, Year = 2018, Month = 1, OverallTrainingTime = 40}
             };
 
-            var yearTimes = FindTraineTimeInYear(clients);
+            var clientsInfo = FindClientMonthCount(clients);
 
-            foreach (var time in yearTimes)
+            foreach (var clientInfo in clientsInfo)
             {
-                Console.WriteLine(time);
+                Console.WriteLine(clientInfo);
             }
         }
 
@@ -36,17 +41,19 @@ namespace Task24
         //Исходная последовательность содержит сведения о клиентах фитнес-центра.
         //Каждый элемент последовательности включает следующие целочисленные поля:
         //<Код клиента>; <Год>; <Номер месяца>; <Продолжительность занятий (в часах)>;
-        //Вывести суммарную продожительность занятий за год, отсортированню по возрастанию.
-        //При наличии одинаковых от меньшего к большему году
+        //Для каждого клиента, присутствующего в исходных данных,
+        //определить общее количество месяцев, в течение которых он посещал занятия(вначале выводить количество месяцев, затем код
+        //клиента). Сведения о каждом клиенте выводить на новой строке и
+        //упорядочивать по возрастанию количества месяцев, а при их равенстве – по возрастанию кода клиента.
         //</summary>
-        static List<string> FindTraineTimeInYear(List<Client> clients)
+        static List<string> FindClientMonthCount(List<Client> clients)
         {
-            var years = (from client in clients
-                         group client by client.Year into yearGroup
-                         orderby yearGroup.Sum(client => client.OverallTrainingTime), yearGroup.Key
-                         select $"{yearGroup.Key} - {yearGroup.Sum(client => client.OverallTrainingTime)}").ToList();
+            var clientsInfo = (from client in clients
+                         group client by client.Id into clientGroup
+                         orderby clientGroup.Count(), clientGroup.Key
+                         select $"Count of month: {clientGroup.Count()} - Id: {clientGroup.Key}").ToList();
 
-            return years;
+            return clientsInfo;
         }
     }
 }
